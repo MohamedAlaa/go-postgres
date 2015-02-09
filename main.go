5172acf8d479e22a91e1c854a6e4dc0f43ffa84f
MohamedAlaa <mohamed.hassan@linio.com>
2015-02-08 22:20:10 -0600

    Postgres: Inserting Data using a Form

--- main.go
+++ main.go
@@ -33,30 +33,51 @@ func main() {
     Layout: "layout",
   }))
 
-  m.Get("/", func(ren render.Render, r *http.Request, db *sql.DB) {
-
-    searchTerm := "%" + r.URL.Query().Get("search") + "%"
-
-    rows, err := db.Query(`SELECT title, author, description FROM books
-      WHERE author ILIKE $1
-      OR author ILIKE $1
-      OR description ILIKE $1`, searchTerm)
-
-    PanicIf(err)
-    defer rows.Close()
-
-    books := []Book{}
-    for rows.Next() {
-      b := Book{}
-      PanicIf(rows.Err())
-      err := rows.Scan(&b.Title, &b.Author, &b.Description)
-      PanicIf(err)
-      books = append(books, b)
-    }
-
-    ren.HTML(200, "books", books)
-
-  })
+  m.Get("/", ShowBooks)
+  m.Get("/create", NewBook)
+  m.Post("/books", Create)
 
   m.Run()
+}
+
+func ShowBooks(ren render.Render, r *http.Request, db *sql.DB) {
+
+  searchTerm := "%" + r.URL.Query().Get("search") + "%"
+
+  rows, err := db.Query(`SELECT title, author, description FROM books
+    WHERE author ILIKE $1
+    OR author ILIKE $1
+    OR description ILIKE $1`, searchTerm)
+
+  PanicIf(err)
+  defer rows.Close()
+
+  books := []Book{}
+  for rows.Next() {
+    b := Book{}
+    PanicIf(rows.Err())
+    err := rows.Scan(&b.Title, &b.Author, &b.Description)
+    PanicIf(err)
+    books = append(books, b)
+  }
+
+  ren.HTML(200, "books", books)
+
+}
+
+func NewBook(ren render.Render) {
+  ren.HTML(200, "create", nil)
+}
+
+func Create(ren render.Render, r *http.Request, db *sql.DB) {
+  rows, err := db.Query("INSERT INTO books (title, author, description) VALUES ($1, $2, $3)",
+    r.FormValue("title"),
+    r.FormValue("author"),
+    r.FormValue("description"),
+  )
+
+  PanicIf(err)
+  defer rows.Close()
+
+  ren.Redirect("/")
 }
